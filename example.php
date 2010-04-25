272fa7c7dad92aae98ea26e2732f34c76d727835
Aleksandar Kolundzija <akolundzija@gmail.com>
2010-04-25 08:33:17 -0400

    Moved JS to the bottom of the page (cause that's where it belongs)

--- example.php
+++ example.php
@@ -4,30 +4,32 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-<?
-	// create bundler for JavaScript files
-	$jsBundler = new FileBundler(array(
-		"type"=>"js",
-		"debugMode"=>true,
-		"approot"=>"/www",
-		"sourceDir"=>"/js",
-		"bundleDir"=>"/bundles"
-	)); 
-
-	// add single file to bundle
-	$jsBundler->addFile("/scripts/myScript.js");
-
-	// add multiple files to bundle
-	$jsFiles = array("/scripts/otherScript.js", "/scripts/yetAnotherScript.js");
-	$jsBundler->addFiles($jsFiles);
-
-	// create new (or reuse if existing) bundle
-	$jsBundler->writeBundle();
-?>
+<title>JavaScript (and CSS) Bundler.  Example page.</title>
 </head>
 
 <body>
+	<h1>Bundler</h1>
+	<?
+		// create bundler for JavaScript files
+		$jsBundler = new FileBundler(array(
+			"type"=>"js",
+			"debugMode"=>true,
+			"approot"=>"/www",
+			"sourceDir"=>"/js",
+			"bundleDir"=>"/bundles"
+		)); 
+
+		// add single file to bundle
+		$jsBundler->addFile("/scripts/myScript.js");
+
+		// add multiple files to bundle
+		$jsFiles = array("/scripts/otherScript.js", "/scripts/yetAnotherScript.js");
+		$jsBundler->addFiles($jsFiles);
+
+		// create new (or reuse if existing) bundle
+		$jsBundler->writeBundle();
+	?>
+	
 </body>
 </html>
 
